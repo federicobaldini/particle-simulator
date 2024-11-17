e2d46536f7233ad575e9974d591b8b3cb03d1934
Federico Baldini <federico.baldini7@gmail.com>
2024-11-17 12:18:39 +0100

    Update ResonanceType.cpp

--- src/ResonanceType.cpp
+++ src/ResonanceType.cpp
@@ -1,19 +1,27 @@
 #include "ResonanceType.h"
 #include <iostream>
 
-// Constructor initializing base properties and resonance width
+// Costruttore che inizializza le proprietà di base della particella (nome, massa e carica)
+// e la larghezza della risonanza.
+// name: nome della particella
+// mass: massa della particella
+// charge: carica elettrica della particella
+// width: larghezza della risonanza
 ResonanceType::ResonanceType(const std::string &name, double mass, int charge, double width)
     : ParticleType(name, mass, charge), fWidth(width) {}
 
-// Returns resonance width
+// Metodo che restituisce la larghezza della risonanza.
+// return: valore della larghezza di risonanza (fWidth)
 double ResonanceType::GetWidth() const
 {
   return fWidth;
 }
 
-// Prints particle properties, including resonance width
+// Metodo per stampare tutte le proprietà della particella, inclusa la larghezza di risonanza.
+// Chiama il metodo Print della classe base per stampare nome, massa e carica,
+// quindi aggiunge la larghezza della risonanza.
 void ResonanceType::Print() const
 {
-  ParticleType::Print(); // Call to base class method
-  std::cout << "Width: " << fWidth << std::endl;
+  ParticleType::Print();                         // Chiamata al metodo della classe base per stampare le proprietà comuni
+  std::cout << "Width: " << fWidth << std::endl; // Stampa la larghezza della risonanza
 }
