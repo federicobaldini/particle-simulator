3079355366bf9106b7b253138349654d78fdb40c
Federico Baldini <federico.baldini7@gmail.com>
2024-12-07 16:31:47 +0100

    Update Particle.h

--- src/Particle.h
+++ src/Particle.h
@@ -23,6 +23,7 @@ private:
   static int FindParticleType(const std::string &name);
 
   // Metodo per applicare un boost relativistico alla quantità di moto della particella
+  // bx, by, bz: componenti della velocità relativistica
   void Boost(double bx, double by, double bz);
 
 public:
@@ -38,6 +39,8 @@ public:
   int GetParticleTypeIndex() const;
 
   // Metodo statico per accedere ad un tipo di particella dato il suo indice
+  // index: indice del tipo di particella
+  // return: puntatore al tipo di particella o nullptr se l'indice non è valido
   static const ParticleType *GetParticleType(int index);
 
   // Metodi per accedere alle componenti della quantità di moto
@@ -53,9 +56,11 @@ public:
   static void AddParticleType(const std::string &name, double mass, int charge, double width = 0);
 
   // Metodo per impostare il tipo di particella usando il nome
+  // name: nome del tipo di particella
   void SetParticleTypeIndex(const std::string &name);
 
   // Metodo per impostare il tipo di particella usando l'indice
+  // index: indice del tipo di particella
   void SetParticleTypeIndex(int index);
 
   // Metodo per stampare le proprietà della particella.
@@ -67,18 +72,21 @@ public:
   void SetPulse(double px, double py, double pz);
 
   // Metodo per ottenere la massa della particella in base al suo tipo
+  // return: massa della particella o 0 se l'indice non è valido
   double GetMass() const;
 
   // Metodo per ottenere l'energia totale della particella usando la relazione relativistica
+  // return: energia totale della particella
   double GetEnergy() const;
 
   // Calcola la massa invariante con un'altra particella
-  // other: altra particella per il calcolo
+  // other: altra particella
+  // return: massa invariante
   double InvariantMass(const Particle &other) const;
 
   // Simula il decadimento della particella in due particelle figlie
   // dau1, dau2: particelle figlie risultanti dal decadimento
-  // return: stato del decadimento (successo o fallimento)
+  // return: stato del decadimento (0: successo, 1: errore di massa zero, 2: massa insufficiente)
   int Decay2Body(Particle &dau1, Particle &dau2) const;
 };
 
